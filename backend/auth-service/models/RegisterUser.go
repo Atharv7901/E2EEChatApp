22ef4a4ce9e62bc96d9ab519d3f3faa38aeb1d74
Atharv7901 <atharvshivarkar@gmail.com>
2025-02-19 22:45:30 +0530

    changed user model

--- backend/auth-service/models/RegisterUser.go
+++ backend/auth-service/models/RegisterUser.go
@@ -3,9 +3,10 @@ package models
 import "time"
 
 type RegisterUser struct {
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"createdAt"`
+	FirstName    string    `json:"firstName"`
+	LastName     string    `json:"lastName"`
+	Email        string    `json:"email"`
+	Password     string    `json:"password"`
+	CreatedAt    time.Time `json:"createdAt"`
+	LastLoggedIn time.Time `json:"lastLoggedIn"`
 }
