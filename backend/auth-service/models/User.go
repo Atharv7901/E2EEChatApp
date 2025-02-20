4a0e559de38762f37448bf8e5d796405bb97a9f3
Atharv7901 <atharvshivarkar@gmail.com>
2025-02-21 01:44:41 +0530

    user model

---
+++ backend/auth-service/models/User.go
@@ -0,0 +1,8 @@
+package authmodels
+
+type SignupRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
