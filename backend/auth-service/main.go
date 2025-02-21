556304b22318083f0925a06e2b1e88b144f4b904
Atharv7901 <atharvshivarkar@gmail.com>
2025-02-22 01:49:57 +0530

    implemented main.go

--- backend/auth-service/main.go
+++ backend/auth-service/main.go
@@ -4,6 +4,10 @@ import (
 	"log"
 
 	"github.com/Atharv7901/E2EEChatApp/config"
+	authHandler "github.com/Atharv7901/E2EEChatApp/handler"
+	authroutes "github.com/Atharv7901/E2EEChatApp/routes"
+	authservice "github.com/Atharv7901/E2EEChatApp/service"
+	authstore "github.com/Atharv7901/E2EEChatApp/store"
 )
 
 func main() {
@@ -15,8 +19,13 @@ func main() {
 	config.ConnectionTest(db)
 
 	//initialize service, store and handlers
+	authStore := authstore.NewAuthStore(db)
+	authService := authservice.NewAuthService(authStore)
+	authHandler := authHandler.NewAuthHandler(authService)
 
 	//routes
-
-	//start the servers
+	server := authroutes.NewAuthServer(":8080", db)
+	if err := server.Run(authHandler); err != nil {
+		log.Fatal(err)
+	}
 }
